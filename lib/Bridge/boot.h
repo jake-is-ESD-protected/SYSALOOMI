d5fc6a4a76e1044673f3e8792f0989fe2e4fd151
Jake-is-ESD-protected <jt@tschavoll.at>
2023-05-27 16:47:02 +0200

    add cli and general bridge submodules

---
+++ lib/Bridge/boot.h
@@ -0,0 +1,12 @@
+#ifndef _BOOT_H_
+#define _BOOT_H_
+
+/* Boot up routine
+* standard initialization of all peripherals
+* creation of all undying tasks
+* retrieve info from persistent memory
+*/
+
+#include "Arduino.h"
+
+#endif
---
+++ lib/Bridge/queues.h
@@ -0,0 +1,10 @@
+#ifndef _QUEUES_H_
+#define _QUEUES_H_
+
+/* Queue exposure point
+* Make queues visible to other submodules for information sharing
+*/
+
+#include "Arduino.h"
+
+#endif
