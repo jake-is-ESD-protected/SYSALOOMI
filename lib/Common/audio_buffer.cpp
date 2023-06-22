9ed2659df60e39342c0a0461f8ac92a9fcd892e8
Jake-is-ESD-protected <jt@tschavoll.at>
2023-06-22 11:14:10 +0200

    add buffer type and init

--- lib/Common/audio_buffer.cpp
+++ lib/Common/audio_buffer.cpp
@@ -1,1 +1,14 @@
 #include "audio_buffer.h"
+
+err_t initBuf(buffer_handle_t* h_buf, buffer_t* p_mem){
+    if(h_buf == NULL || p_mem == NULL){
+        return e_mem_null;
+    }
+    if(h_buf->len == 0){
+        return e_is_zero;
+    }
+
+    h_buf->stop = h_buf->len - 1;
+    h_buf->mem = p_mem;
+    return e_no_err;
+}
--- lib/Common/audio_buffer.h
+++ lib/Common/audio_buffer.h
@@ -4,5 +4,31 @@
 /* Audio IO and storage */
 
 #include "Arduino.h"
+#include "bridge.h"
+
+typedef struct sample{
+    uint16_t l = 0;
+    uint16_t r = 0;
+}sample_t;
+
+typedef struct buffer{
+    sample_t sample;
+}buffer_t;
+
+typedef struct buffer_handle{
+    uint32_t len = 0;
+    uint32_t start = 0;
+    uint32_t stop = 0;
+    buffer_t* mem = NULL;
+}buffer_handle_t;
+
+/* Buffer initializer
+* PARAMS
+* `buffer_handle_t* h_buf`: buffer handle describing buffer
+* `buffer_t* p_mem`: pointer to static memory buffer
+* RETURNS
+* `err_t` status, `e_no_err` if OK
+ */
+err_t initBuf(buffer_handle_t* h_buf, buffer_t* p_mem);
 
 #endif
