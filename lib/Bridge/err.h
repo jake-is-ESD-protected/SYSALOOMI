e2d0a6283e16c7fa459ca081a88756034199d0b1
Jake-is-ESD-protected <jt@tschavoll.at>
2023-06-22 13:23:42 +0200

    more error types

--- lib/Bridge/err.h
+++ lib/Bridge/err.h
@@ -3,11 +3,18 @@
 
 /* Error interpreter and handling */
 
+#define INFO    0
+#define WARNING 1
+#define ERROR   2
+#define SEVERE  3
+
 typedef enum err{
     e_no_err,
     e_mem_null,
     e_is_zero,
+    e_param,
     e_peripheral_block,
+    e_core_fail,
 }err_t;
 
 /* General error handler
